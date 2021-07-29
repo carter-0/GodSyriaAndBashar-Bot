473e4845b9f8a9e94b0faa016de9b2ce98be9a20
carter <carter>
2021-07-29 08:46:27 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from discord.utils import get
+import discord
+
+client = discord.Client()
+
+def repeat(voice, audio, channel):
+	if channel and not voice.is_playing():
+		audio = discord.FFmpegPCMAudio('gsb.mp3')
+		voice.play(audio, after=lambda e: repeat(voice, audio, channel))
+		voice.is_playing()
+
+@client.event
+async def on_message(message):
+	if ";;gsb" in message.content:
+		channel = message.author.voice.channel
+		voice = get(client.voice_clients, guild=message.channel.guild)
+
+		if voice and voice.is_connected():
+			await voice.move_to(channel)
+		else:
+			voice = await channel.connect()
+
+		if channel and not voice.is_playing():
+			audio = discord.FFmpegPCMAudio('gsb.mp3')
+			voice.play(audio, after=lambda e: repeat(voice, audio, channel))
+			voice.is_playing()
+
+client.run("ODcwMDM5NDY5MzQzMDAyNjU1.YQG9Zg.smEZu1DMyTa1_fW6GWE-YZr3YgU")
